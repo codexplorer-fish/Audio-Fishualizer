42ff60275f3ee60b337648324222c51a03367601
Codexplorer <codexplorer@hotmail.com>
2024-11-01 11:38:53 -0700

    fixed page "white space" on mobile

--- analyse-animate.js
+++ analyse-animate.js
@@ -14,13 +14,17 @@ exporting presets page, preset version attached! this will be complicated
 - scrollable focus div, text inputs: copy save, load save, copy preset, load preset. Use same functions already used.
 - change format, v1 as last value. append to copy inputs, and splice out and read when loading. then modify save as necessary.
 fix denied permissions handling, possibly even unsupported error handling - user input, user permission denial, application permission denial
-
+crop focus divs and remove audio controls margins
 
 
 scroll when overflow - resize main sidebar, then scroll; snap to floor sidebar (bottom = 0), then scroll
+- if overflow, snap or resize
+- if still overflow, scroll.
 
-crop focus divs and remove audio controls margins
 if mobile mode, toggle hiddeness if only doc is clicked
+keyboard mode - a way to open the sidebars
+
+"click anywhere to start"
 
 way more sliders per style
 style for analysing (20-20k, log style)
@@ -36,11 +40,8 @@ add/remove from background, basically lets you stack styles
 - outline for styles? OR JUST MAKE SLIGHTLY BIGGER OUTLINE
 ... then BACKGROUND PRESET SAVE/LOAD :,)
 
-keyboard mode - a way to open the sidebars
-mobile mode - only clicks are registered, not mousemove. think about it. Also, focusdiv margins need to be cropped
-
 maybe:
-label tag for accessibility purposes
+label tags for accessibility purposes
 save last unset preset for refresh
 
 
--- sidebar-positioning.js
+++ sidebar-positioning.js
@@ -1,5 +1,9 @@
 function initSidebarPositioning() {
     const main = document.getElementById("mainSidebar")
     const collapsingSidebars = document.getElementsByClassName("collapsingSidebar")
+    /*
+    - if overflow, snap or resize
+    - if still overflow, scroll.
+    */
 }
 initSidebarPositioning()
